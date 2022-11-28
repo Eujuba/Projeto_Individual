170c992e62279646c8644599d699a6f5b9c6d270
Juliana Godoy <110927788+Eujuba@users.noreply.github.com>
2022-11-28 18:16:08 -0300

    pasta public atualizada

--- site/site/public/js/funcoes.js
+++ site/site/public/js/funcoes.js
@@ -1,43 +1,28 @@
 // sessão
 function validarSessao() {
-    // aguardar();
 
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var persona = sessionStorage.PERSONA_USUARIO;
+
 
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null) {
-        // window.alert(`Seja bem-vindo, ${nome}!`);
+    if (email != undefined && nome != undefined && fkIntegrante != undefined) {
+        window.alert(`Seja bem-vindo, ${nome}!`);
         b_usuario.innerHTML = nome;
+        b_fkIntegrante.innerHTML = persona;
+        
 
-        // finalizarAguardar();
+
     } else {
-        window.location = "../login.html";
+        window.location = "../login/login.html";
     }
 }
 
 function limparSessao() {
-    // aguardar();
     sessionStorage.clear();
-    // finalizarAguardar();
-    window.location = "../login.html";
-}
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.innerHTML = texto;
-    }
+    window.location = "../login/login.html";
 }
 
 
