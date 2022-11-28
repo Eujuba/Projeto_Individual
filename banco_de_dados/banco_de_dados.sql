009e5ba04a0ddab91e756108e42a761a9978af89
Juliana Godoy <110927788+Eujuba@users.noreply.github.com>
2022-11-28 18:22:46 -0300

    banco de dados atualizado

---
+++ banco_de_dados/banco_de_dados.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE IF NOT EXISTS beatles;
+USE beatles;
+
+CREATE TABLE  IF NOT EXISTS integrante (
+	idIntegrante int primary key auto_increment,
+    nome varchar (45)
+);
+
+insert into integrante values
+(null, 'John Lennon'),
+(null, 'Paul McCartney'),
+(null, 'Ringo Starr'),
+(null, 'George Harrison');
+
+CREATE TABLE  IF NOT EXISTS usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+    fkIntegrante int, 
+    foreign key (fkIntegrante) references integrante (idIntegrante)
+);
+
+
+CREATE TABLE  IF NOT EXISTS musica_favorita (
+    nome varchar (45),
+    fkUsuario int,
+    foreign key (fkUsuario) references usuario (id)
+);
+
+CREATE TABLE IF NOT EXISTS comentario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+Insert into integrante values
+(null, 'Paul McCartney'),
+(null, 'John Lennon'),
+(null, 'Ringo Starr'),
+(null, 'George Harrison');
+
+select* from usuario;
+select*from integrante;
